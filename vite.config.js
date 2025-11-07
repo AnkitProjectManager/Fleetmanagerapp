74e77ca26b3677aacebda4c6a3014aac6e12f075
DJ1992007 <writexankit@gmail.com>
2025-11-08 00:25:22 +0530

    fix: Correct index.html path and revert vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite'
-import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from "tailwindcss"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
   },
   server: {
     proxy: {
