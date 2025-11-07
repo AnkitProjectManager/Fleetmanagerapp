63c02f62b0f83f734bcff6702baa8e406d2d6c84
DJ1992007 <writexankit@gmail.com>
2025-11-07 23:38:41 +0530

    feat(backend): isolate backend code by removing frontend folder

--- frontend/src/components/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ModernDashboard from './dashboard/ModernDashboard';
-
-const Dashboard = () => {
-  return <ModernDashboard />;
-};
-
-export default Dashboard;
--- frontend/src/components/Vehicles.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ModernVehicles from './vehicles/ModernVehicles';
-
-const Vehicles = () => {
-  return <ModernVehicles />;
-};
-
-export default Vehicles;
