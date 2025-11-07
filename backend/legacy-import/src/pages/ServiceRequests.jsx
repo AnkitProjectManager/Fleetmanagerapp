3c1811a49fd86aa9d4ffdf91238f9e09a9a7b30e
DJ1992007 <writexankit@gmail.com>
2025-11-07 23:37:46 +0530

    feat(frontend): isolate frontend code by removing backend folder

--- backend/legacy-import/src/pages/ServiceRequests.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ModernServiceRequests from './servicerequests/ModernServiceRequests';
-
-const ServiceRequests = () => {
-  return <ModernServiceRequests />;
-};
-
-export default ServiceRequests;
